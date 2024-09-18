3aaf16a726d607a289d8e6bf555c818971be7219
hmetcalfe1 <h.metcalfe1@gmail.com>
2024-09-18 14:04:45 +0100

    Add trait density plots
    ADd functionality to create plots showing the distribution of a trait within a community. Options for both continuous and discrete traits

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Generic R scripts for various plant community analyses
 
 # functional_diversity.R
 Calculates the functional diversity (following the method of Petchey & Gaston (2007)). Trait data are scaled and centred, and a Gower dissimilarity matrix is computed using the vegan package (Oksanen et al 2019). Hierarchical cluster analysis on the dissimilarity matrix forms a tree (using the ape package, Paradis & Schliep, 2019) where the branch length represents functional dissimilarity and each node is a species. Functional diversity of the community is calculated as the sum of the total branch length present in that community (using picante pacage).
+
+# trait_density_plots.R
+Creates plots showing the distribution of a trait within a community. There are two options shown here, one for creating a density plot of a continuous trait e.g. plant height, the other for discrete traits e.g. Ellenberg N
---
+++ scripts/trait_density_plots.R
@@ -0,0 +1,63 @@
+# ---
+# title:trait_density_plots
+# description:Create plots showing the distribution of a trait within a community. There are two options shown here, one for creating a density plot of a continuous trait e.g. plant height, the other for discrete traits e.g. Ellenberg N
+# author: helen metcalfe
+# date created: 2024-09-18
+# ---
+
+library("ggplot2")
+library("dplyr")
+
+# Load data -------------------------------------------------------------------
+# Load a csv of trait data - Rows should be species, and traits as columns
+
+traits <- read.csv("traits.csv")
+
+
+## Load csv of species observations - There should be a column of species names and a second column containing the counts. (formatting of species names should match the names in traits.csv)
+
+species_counts <- read.csv("species_counts.csv")
+
+
+# Merce trait and count data ---------------------------------------------------
+
+count_traits <- merge(species_counts, traits, by = "species_name")
+
+
+# Create Plots -----------------------------------------------------------------
+
+## For continuous traits -------------------------------------------------------
+
+### Calculate weighting for each species ---------------------------------------
+
+count_traits$weight <-
+  as.numeric(count_traits$count) / sum(as.numeric(count_traits$count))
+
+
+### Draw plot ------------------------------------------------------------------
+ggplot(na.omit(count_traits), aes(continuous_trait)) +
+  geom_density(aes(x = continuous_trait_1, weight = weight)) +
+  scale_x_continuous()
+
+
+## For discrete traits ---------------------------------------------------------
+
+### Summarize the data ---------------------------------------------------------
+
+discrete_summ <- count_traits %>%
+  group_by(as.factor(eval(as.name(discrete_trait)))) %>%
+  summarise(n = sum(count))
+
+discrete_summ$density <- discrete_summ$n / sum(discrete_summ$n)
+
+
+### Draw plot ------------------------------------------------------------------
+
+ggplot() +
+  geom_bar(
+    data = discrete_summ,
+    aes_string(x = trait, y = density),
+    alpha = 0.4,
+    stat = "identity",
+    position = position_dodge()
+  )
